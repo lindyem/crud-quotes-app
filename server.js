6f9abe9dfd39dc8813e852b4e4794209653d29f9
Lindy Merwin <lindymerwin@Lindys-Air.attlocal.net>
2021-02-22 09:25:59 -0800

    Set up DB

--- server.js
+++ server.js
@@ -4,20 +4,29 @@ const app = express();
 
 const MongoClient = require('mongodb').MongoClient
 
-MongoClient.connect('mongodb+srv://lindy:blueman1a@cluster0.uexxx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', (err, client) => {
-  console.log('im in DB')
-})
+MongoClient.connect('mongodb+srv://lindy:blueman1a@cluster0.uexxx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+{
+  useUnifiedTopology: true
+  })
+  .then(client => {
+  console.log('Im in DB')
+    const db = client.db('star-wars-quotes')
+    
+    app.listen(3000, function () {
+      console.log('listening on 3000')
+    })
+    
+    app.use(bodyParser.urlencoded({ extended: true }))
+    
+    app.get('/', (req, res) => {
+      res.sendFile(__dirname + '/index.html')
+    })
+    
+    app.post('/quotes', (req, res) => {
+      console.log(req.body)
+    })
+  }) 
+  .catch(console.error)
 
-app.listen(3000, function () {
-  console.log('listening on 3000')
-})
 
-app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-})
-
-app.post('/quotes', (req, res) => {
-  console.log(req.body)
-})
