183073453812c3c1a8d43e2cdcc4ffc65248cc55
Lindy Merwin <lindymerwin@Lindys-Air.attlocal.net>
2021-02-22 10:28:49 -0800

    Create EJS file and generate HTML with quotes

--- server.js
+++ server.js
@@ -11,19 +11,38 @@ MongoClient.connect('mongodb+srv://lindy:blueman1a@cluster0.uexxx.mongodb.net/my
   .then(client => {
   console.log('Im in DB')
     const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
     
     app.listen(3000, function () {
       console.log('listening on 3000')
     })
-    
+
+    // ========================
+      // Middlewares
+    // ========================
     app.use(bodyParser.urlencoded({ extended: true }))
+    app.set('view engine', 'ejs')
+
     
+
+     // ========================
+       // Routes
+     // ========================
+
     app.get('/', (req, res) => {
-      res.sendFile(__dirname + '/index.html')
+      db.collection('quotes').find().toArray()
+        .then(quotes => {
+          res.render('index.ejs', { quotes: results })
+        })
+        .catch()
     })
-    
+
     app.post('/quotes', (req, res) => {
-      console.log(req.body)
+      quotesCollection.insertOne(req.body)
+        .then(result => {
+          res.redirect('/')
+      })
+      .catch()
     })
   }) 
   .catch(console.error)
